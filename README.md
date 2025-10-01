3560c388c364a46bc42045c3e06341d601b3cf53
Nelson Rengifo <nrengifo3107@gmail.com>
2025-10-01 19:26:24 -0400

    updates

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Pomodoro Timer — Requirements (MVP)
+
+# seconds need to always show 2 digits. 09 instead of 9.
+
+# prevent logic if seconds == 0.
--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,56 @@
 const timer = document.getElementById("time");
-const startButton = document.getElementById("start");
+const button = document.getElementById("start");
+const resetButton = document.getElementById("reset");
 
-startButton.addEventListener("click", function()
+let MY_INTERVAL;
+
+function startTimer()
 {
-    
-    let time =  timer.textContent; // "25:00"
-    time = parseInt(time.slice(0,2) + time.slice(3)) 
-    console.log(typeof time)
-    
-    // create loop from minutes to seconds and vs.
-    // use timer.textcontent to update actual website time.
+    if(timer.textContent == "00:00")
+    {
+        clearInterval(MY_INTERVAL);
+        button.textContent = "Start";
+        return;
+    }
+
+    let currentTime =  timer.textContent;
+    let colon = currentTime.indexOf(":");
+    let minutes = parseInt(currentTime.slice(0,colon));
+    let seconds = parseInt(currentTime.slice(colon + 1));
+    let totalSeconds = (minutes * 60) + seconds;
+
+    totalSeconds--;
+
+    let newTime = totalSeconds;
+    minutes = String(Math.floor(newTime / 60)); 
+    seconds = String(totalSeconds % 60);
+
+    minutes = minutes.padStart(2, "0");
+    seconds = seconds.padStart(2, "0");
+    newTime = minutes + ":" + seconds;
+
+    timer.textContent = newTime;
+};
+
+button.addEventListener("click", function()
+{
+    if(button.textContent == "Start")
+    {
+        button.textContent = "Pause";
+        MY_INTERVAL = setInterval(startTimer, 1000);
+    }
+
+    else
+    {
+        clearInterval(MY_INTERVAL);
+        button.textContent = "Start";
+    }
+});
+
+
+resetButton.addEventListener("click", function()
+{
+    clearInterval(MY_INTERVAL);
+    button.textContent = "Start";
+    timer.textContent = "25:00";
 });
